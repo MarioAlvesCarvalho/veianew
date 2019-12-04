05639d9a78ebfa256c8f7f7cf7f1fb6cfbf07ffd
“MarioAlvesCarvalho” <mario.alves.souza@outlook.com>
2019-12-04 15:06:55 -0300

    -d $VEIANEW

--- 002.sh
+++ 002.sh
@@ -1,12 +1,12 @@
+GNU nano 2.3.1                                         Arquivo: 002.sh                                                                              Modificado
+
 #!/bin/bash
 #Data alteração 19338
 wget -N -v --tries=1 -P $VEIANEW https://github.com/MarioAlvesCarvalho/veianew/archive/master.zip &&
 chmod -R 777  $VEIANEW/master.zip &&
 chmod +x $VEIANEW/master.zip &&
-unzip -oq $VEIANEW/master.zip &&
-sleep 3 &&
+unzip -oq $VEIANEW/master.zip -d $VEIANEW &&
 sudo cp -rf $VEIANEW/veianew-master/* $VEIANEW/ &&
-sleep 2 &&
 sudo rm -rf $VEIANEW/master.zip &&
 sudo rm -rf $VEIANEW/veianew-master &&
 echo -e "" &&
