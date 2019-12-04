72b6cf853821a1c55403ef86f66725d93b8c3a70
“MarioAlvesCarvalho” <mario.alves.souza@outlook.com>
2019-12-04 11:23:11 -0300

    Criando arqquivo 002.sh. alterando opção 002

---
+++ 002.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#Data alteração 19338
+wget -N -v --tries=1 -P $VEIANEW https://github.com/MarioAlvesCarvalho/veianew/archive/master.zip &&
+unzip -oq $VEIANEW/master.zip &&
+sudo cp -rf $VEIANEW/veianew-master/* $VEIANEW/ ;;
