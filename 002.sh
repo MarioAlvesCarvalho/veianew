0fa5d85fe6b9ef5203e56db46d6fcb8e572dae21
“MarioAlvesCarvalho” <mario.alves.souza@outlook.com>
2019-12-04 12:49:35 -0300

    chmod 777 002.sh chmod +x  002.sh

--- 002.sh
+++ 002.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 #Data alteração 19338
 wget -N -v --tries=1 -P $VEIANEW https://github.com/MarioAlvesCarvalho/veianew/archive/master.zip &&
+chmod -R 777  $VEIANEW/master.zip &&
+chmod +x $VEIANEW/master.zip &&
 unzip -oq $VEIANEW/master.zip &&
-sudo cp -rf $VEIANEW/veianew-master/* $VEIANEW/ &&
+sudo cp -rf $VEIANEW/veianew-master/* $VEIANEW/ 
