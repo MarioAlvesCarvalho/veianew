2419e7240ba346800fa2addf205403b8183e1c81
“MarioAlvesCarvalho” <mario.alves.souza@outlook.com>
2019-12-04 12:39:29 -0300

    && no final da linha 3

--- 002.sh
+++ 002.sh
@@ -2,4 +2,4 @@
 #Data alteração 19338
 wget -N -v --tries=1 -P $VEIANEW https://github.com/MarioAlvesCarvalho/veianew/archive/master.zip &&
 unzip -oq $VEIANEW/master.zip &&
-sudo cp -rf $VEIANEW/veianew-master/* $VEIANEW/ 
+sudo cp -rf $VEIANEW/veianew-master/* $VEIANEW/ &&
