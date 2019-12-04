b1b4505b91648d4e4fca84474583a786414a150d
“MarioAlvesCarvalho” <mario.alves.souza@outlook.com>
2019-12-04 13:15:23 -0300

    chamando veianew_estruturado no 002.sh
    tirando segunda linha export do veianew

--- 002.sh
+++ 002.sh
@@ -4,4 +4,6 @@ wget -N -v --tries=1 -P $VEIANEW https://github.com/MarioAlvesCarvalho/veianew/a
 chmod -R 777  $VEIANEW/master.zip &&
 chmod +x $VEIANEW/master.zip &&
 unzip -oq $VEIANEW/master.zip &&
-sudo cp -rf $VEIANEW/veianew-master/* $VEIANEW/ 
+sudo cp -rf $VEIANEW/veianew-master/* $VEIANEW/
+
+veianew_estruturado
