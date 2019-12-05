fd38fbc1d7948b861ccd871fc61a6ebdab686812
“MarioAlvesCarvalho” <mario.alves.souza@outlook.com>
2019-12-05 09:16:17 -0300

    5.0.14

--- 002.sh
+++ 002.sh
@@ -2,6 +2,7 @@ GNU nano 2.3.1                                         Arquivo: 002.sh          
 
 #!/bin/bash
 #Data alteração 19338
+
 wget -N -v --tries=1 -P $VEIANEW https://github.com/MarioAlvesCarvalho/veianew/archive/master.zip &&
 chmod -R 777  $VEIANEW/master.zip &&
 chmod +x $VEIANEW/master.zip &&
