f295b13177884aebaabe95d23972f868d9c71620
“MarioAlvesCarvalho” <mario.alves.souza@outlook.com>
2019-12-27 13:23:34 -0300

    estruturando 02.sh

--- 02.sh
+++ 02.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 #Data alteração 19361
 
-${COMANDO4} && sleep 4 ; mv -f  ${HTDOCS}/modulo_compilado ${HTDOCS}/modulo_compilado_OLD ; sleep 3 ; mkdir ${HTDOCS}/modulo_compilado ; chmod -R 777 ${HTDOCS}/modulo_compilado ; rsync -ravzpt ${SERVER_VN_MODULO_COMPILADO}/* ${HTDOCS}/modulo_compilado/ && umount ${SERVER_VN_MODULO_COMPILADO}/ && rm -rf ${HTDOCS}/modulo_compilado_OLD && sleep 1 && echo -e "" ; echo -e "" ; echo -e 'Atualização do Modulo_Compilado concluída com sucesso!!!' ; echo -e "" ; echo -e "" ; sleep 2 ; veianew &&
+${COMANDO4} &&
+sleep 4 &&
+mv -f  ${HTDOCS}/modulo_compilado ${HTDOCS}/modulo_compilado_OLD &&
+sleep 3 &&
+mkdir ${HTDOCS}/modulo_compilado &&
+chmod -R 777 ${HTDOCS}/modulo_compilado &&
+rsync -ravzpt ${SERVER_VN_MODULO_COMPILADO}/* ${HTDOCS}/modulo_compilado/ &&
+umount ${SERVER_VN_MODULO_COMPILADO}/ &&
+rm -rf ${HTDOCS}/modulo_compilado_OLD &&
+sleep 1 &&
+echo -e "" &&
+echo -e "" &&
+echo -e 'Atualização do Modulo_Compilado concluída com sucesso!!!' &&
 echo -e "" &&
 echo -e "" &&
 echo -e "" &&
