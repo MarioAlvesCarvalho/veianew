29230e8f65c9da41ddc36b4ef2b79c8ff03f003c
“MarioAlvesCarvalho” <mario.alves.souza@outlook.com>
2019-12-27 13:30:20 -0300

    com log

--- 02.sh
+++ 02.sh
@@ -1,14 +1,30 @@
 #!/bin/bash
 #Data alteração 19361
-
+echo -e "linha 1"
 ${COMANDO4} &&
+
+echo -e "linha 2"
 sleep 4 &&
-mv -f  ${HTDOCS}/modulo_compilado ${HTDOCS}/modulo_compilado_OLD &&
+
+echo -e "linha 3"
+mv -f ${HTDOCS}/modulo_compilado ${HTDOCS}/modulo_compilado_OLD &&
+
+echo -e "linha 4"
 sleep 3 &&
+
+echo -e "linha 5"
 mkdir ${HTDOCS}/modulo_compilado &&
+
+echo -e "linha 6"
 chmod -R 777 ${HTDOCS}/modulo_compilado &&
+
+echo -e "linha 7"
 rsync -ravzpt ${SERVER_VN_MODULO_COMPILADO}/* ${HTDOCS}/modulo_compilado/ &&
+
+echo -e "linha 8"
 umount ${SERVER_VN_MODULO_COMPILADO}/ &&
+
+echo -e "linha 9"
 rm -rf ${HTDOCS}/modulo_compilado_OLD &&
 sleep 1 &&
 echo -e "" &&
