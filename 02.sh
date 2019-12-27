8adbc320f5078bd50c6bc41896606fe96ff860aa
“MarioAlvesCarvalho” <mario.alves.souza@outlook.com>
2019-12-27 14:33:13 -0300

    comando direto sem variavel.

--- 02.sh
+++ 02.sh
@@ -1,35 +1,17 @@
 #!/bin/bash
 #Data alteração 19361
-echo -e "linha 1"
+
 ${COMANDO4} &&
-
-echo -e "linha 2"
 sleep 4 &&
-
-echo -e "linha 3"
 mv -f ${HTDOCS}/modulo_compilado ${HTDOCS}/modulo_compilado_OLD &&
-
-echo -e "linha 3"
 sleep 3 &&
-
-echo -e "linha 4"
 mkdir ${HTDOCS}/modulo_compilado &&
-wait &&
-
-echo -e "linha 5"
 chmod -R 777 ${HTDOCS}/modulo_compilado &&
-
-echo -e "linha 6"
 sleep 3 &&
-
-echo -e "linha 7"
-rsync -ravzpt ${SERVER_VN_MODULO_COMPILADO}/* ${HTDOCS}/modulo_compilado/
-wait &
-
-echo -e "linha 8"
-##umount ${SERVER_VN_MODULO_COMPILADO}/ &&
-
-echo -e "linha 9"
+rsync -ravzpt /Zanthus/Zeus/path_comum/0.66/SCRIPTS_VEIANEW/MODULO_COMPILADO/* /usr/local/apache2/htdocs/modulo_compilado/
+sleep 5 &&
+#rsync -ravzpt ${SERVER_VN_MODULO_COMPILADO}/* ${HTDOCS}/modulo_compilado/
+umount ${SERVER_VN_MODULO_COMPILADO}/ &&
 rm -rf ${HTDOCS}/modulo_compilado_OLD &&
 sleep 1 &&
 echo -e "" &&
