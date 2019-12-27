1f5f35c318b363dd6d2bc5acb20e396b17372bd9
“MarioAlvesCarvalho” <mario.alves.souza@outlook.com>
2019-12-27 11:06:29 -0300

    Ajustando 04.sh v5.0.25_BETA

--- 04.sh
+++ 04.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
 #Data alteração 19361
 
-/etc/init.d/memcached restart && sleep 1 ; echo -e "" ; echo -e "" ;  echo -e 'Reinicialização do MEMCACHED concluida com sucesso!!!' ; echo -e "" ; echo -e "" ; sleep 3 ; veianew ;;
+/etc/init.d/memcached restart &&
+sleep 1 &&
+echo -e "" &&
+echo -e "" &&
+echo -e 'Reinicialização do MEMCACHED concluida com sucesso!!!' &&
+echo -e "" &&
+echo -e "" &&
+sleep 3 &&
+echo -e "" &&
+echo -e "" &&
+echo -e "" &&
+echo -e "" &&
+echo -e "" &&
+echo -e "****************" &&
+echo -e "Termino do 01.sh" &&
+echo -e "****************" &&
+veianew ;
