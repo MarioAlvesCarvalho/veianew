4acae8cf3ff9a89c043e9d56c23b329c5e80ec9a
“MarioAlvesCarvalho” <mario.alves.souza@outlook.com>
2019-12-27 10:33:04 -0300

    Criando arquivo 01.sh com opção de atualizar veianew

---
+++ 01.sh
@@ -0,0 +1,20 @@
+GNU nano 2.3.1                                         Arquivo: 002.sh                                                                              Modificado
+
+#!/bin/bash
+#Data alteração 19338
+
+wget -N -v --tries=1 -P $VEIANEW https://github.com/MarioAlvesCarvalho/veianew/archive/master.zip &&
+chmod -R 777  $VEIANEW/master.zip &&
+chmod +x $VEIANEW/master.zip &&
+unzip -oq $VEIANEW/master.zip -d $VEIANEW &&
+sudo cp -rf $VEIANEW/veianew-master/* $VEIANEW/ &&
+sudo rm -rf $VEIANEW/master.zip &&
+sudo rm -rf $VEIANEW/veianew-master &&
+echo -e "" &&
+echo -e "" &&
+echo -e "" &&
+echo -e "" &&
+echo -e "" &&
+echo -e "****************" &&
+echo -e "Termino do 01.sh" &&
+echo -e "****************" ;
