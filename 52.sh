c886dd1c87971d7da33e0a34f81a9cf02705cc41
“MarioAlvesCarvalho” <mario.alves.souza@outlook.com>
2019-12-27 13:11:31 -0300

    Criando arquivo 52.sh v5.0.27_BETA

---
+++ 52.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#Data alteração 19361
+
+sudo umount ${SERVER_VN_MODULOPHPPDV}/ &&
+sleep 2 &&
+${COMANDO2} &&
+sleep 2 &&
+sudo php ${HTDOCS}/modulo_compilado/equalizar_pastas.php &&
+sudo zip -r ${HTDOCS}/Modulo_$(ls ${HTDOCS}/*.zip.TEMP).zip * &&
+cp Modulo_$(ls ${HTDOCS}/*.zip.TEMP).zip ${SERVER_VN_MODULOPHPPDV}/ &&
+umount $moduloPHPPDV/&&
+sleep 1 &&
+echo -e "" &&
+echo -e "" &&
+echo -e "" &&
+echo -e "****************" &&
+echo -e "Termino do 52.sh" &&
+echo -e "****************" ;
