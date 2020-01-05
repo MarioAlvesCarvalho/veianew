5d5c55647aed5e190498b79ec2928713b68650f2
Mario Alves de Souza Carvalho <44899267+MarioAlvesCarvalho@users.noreply.github.com>
2020-01-05 16:33:07 -0300

    Update 97.sh

--- 97.sh
+++ 97.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 #Data alteração 19361
 
-$APACHE2/bin/apachectl stop & $APACHE2/bin/apachectl start ; sleep 1 && veianew &&
+$APACHE2/bin/apachectl stop &
+$APACHE2/bin/apachectl start &&
+sleep 1 &&
 echo -e "" &&
 echo -e "" &&
 echo -e "" &&
