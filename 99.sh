98a658c32edaa0bb6b30d2545026b2400a445a8c
Mario Alves de Souza Carvalho <44899267+MarioAlvesCarvalho@users.noreply.github.com>
2020-01-05 16:40:41 -0300

    Update 99.sh

--- 99.sh
+++ 99.sh
@@ -2,12 +2,12 @@
 #Data alteração 19361
 
 $APACHE2/bin/apachectl start ;
- sleep 1 &&
-  /bin/veianew &&
-  echo -e "" &&
-  echo -e "" &&
-  echo -e "" &&
-  echo -e "****************" &&
-  echo -e "Termino do 99.sh" &&
-  echo -e "****************" &&
-  veianew ;
+sleep 1 &&
+/bin/veianew &&
+echo -e "" &&
+echo -e "" &&
+echo -e "" &&
+echo -e "****************" &&
+echo -e "Termino do 99.sh" &&
+echo -e "****************" &&
+veianew ;
