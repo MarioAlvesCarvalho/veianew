29c4fba67802ad1b0dbe6b7d696aaee535fc4fdc
Mario Alves de Souza Carvalho <44899267+MarioAlvesCarvalho@users.noreply.github.com>
2020-01-05 16:12:05 -0300

    Update 009.sh

--- 009.sh
+++ 009.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 #Data alteração 19361
 
-wget -N -v --tries=1 -P ${VEIANEW} https://github.com/MarioAlvesCarvalho/veianew/archive/master.zip && unzip -oq ${VEIANEW}/master.zip && sudo cp -rf ${VEIANEW}/veianew-master/* ${VEIANEW}/ &&
+wget -N -v --tries=1 -P ${VEIANEW} https://github.com/MarioAlvesCarvalho/veianew/archive/master.zip &&
+unzip -oq ${VEIANEW}/master.zip &&
+sudo cp -rf ${VEIANEW}/veianew-master/* ${VEIANEW}/ &&
 echo -e "" &&
 echo -e "" &&
 echo -e "" &&
