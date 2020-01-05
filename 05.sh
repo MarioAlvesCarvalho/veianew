d07e7cfc45585734b0c01316d6913518693e98ea
Mario Alves de Souza Carvalho <44899267+MarioAlvesCarvalho@users.noreply.github.com>
2020-01-05 16:27:02 -0300

    Update 05.sh

--- 05.sh
+++ 05.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 #Data alteração 19361
 
-umount ${SERVER_VN}/* && umount ${SERVER_VN}/* && umount ${SERVER_VN}/* && umount ${SERVER_VN}/* && umount ${SERVER_VN}/* && umount ${SERVER_VN}/* && umount ${SERVER_VN}/* && umount ${SERVER_VN}/* && umount ${SERVER_VN}/* && umount ${SERVER_VN}/* &&
+umount ${SERVER_VN}/* &&
+umount ${SERVER_VN}/* &&
+umount ${SERVER_VN}/* &&
+umount ${SERVER_VN}/* &&
+umount ${SERVER_VN}/* &&
+umount ${SERVER_VN}/* &&
+umount ${SERVER_VN}/* &&
+umount ${SERVER_VN}/* &&
+umount ${SERVER_VN}/* &&
+umount ${SERVER_VN}/* &&
 echo -e "" &&
 echo -e "" &&
 echo -e "" &&
