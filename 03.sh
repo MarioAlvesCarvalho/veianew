80251387aad3e3fc89b7c340798fef9b88112f83
Mario Alves de Souza Carvalho <44899267+MarioAlvesCarvalho@users.noreply.github.com>
2020-01-05 16:26:00 -0300

    Update 03.sh

--- 03.sh
+++ 03.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 #Data alteração 19361
 
-rsync -uv /Zanthus/Zeus/PATH_COMUM/so_r64/* /usr/src/libs_zanthus ; ldconfig ; sleep 1 ; veianew &
+rsync -uv /Zanthus/Zeus/PATH_COMUM/so_r64/* /usr/src/libs_zanthus &&
+ldconfig &&
+sleep 1 &&
 echo -e "" &&
 echo -e "" &&
 echo -e "" &&
