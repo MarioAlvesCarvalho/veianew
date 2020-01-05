4a2859f40266eee5f9fb054621e93ce11deb14d0
Mario Alves de Souza Carvalho <44899267+MarioAlvesCarvalho@users.noreply.github.com>
2020-01-05 16:52:16 -0300

    Update 87.sh

--- 87.sh
+++ 87.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-#Data alteração 19361
+#Data alteração 20005
 
 php $MANAGER/servico/servicolinux.php5 stop &&
 pkill -9 php &&
 php $MANAGER/servico/servicolinux.php5 start &&
 sleep 1 &&
-veianew &&
 echo -e "" &&
 echo -e "" &&
 echo -e "" &&
