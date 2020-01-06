07f3b8695c42d26a51b4476158da11688acf2197
“MarioAlvesCarvalho” <mario.alves.souza@outlook.com>
2020-01-06 15:08:24 -0300

    v5.0.30_BETA

--- 52.sh
+++ 52.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#Data alteração 20005
+#Data alteração 20006
 
 sudo umount ${SERVER_VN_MODULOPHPPDV}/ &&
 sleep 2 &&
-${COMANDO2} &&
-sleep 2 &&
 sudo php ${HTDOCS}/modulo_compilado/equalizar_pastas.php &&
 sudo zip -r ${HTDOCS}/Modulo_$(ls ${HTDOCS}/*.zip.TEMP).zip * &&
+${COMANDO2} &&
+sleep 2 &&
 cp Modulo_$(ls ${HTDOCS}/*.zip.TEMP).zip ${SERVER_VN_MODULOPHPPDV}/ &&
 umount $SERVER_VN_MODULOPHPPDV/ &&
 sleep 1 &&
