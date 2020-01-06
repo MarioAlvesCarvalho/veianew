3c74421afc209200a08e33602394dfd40cb86d11
“MarioAlvesCarvalho” <mario.alves.souza@outlook.com>
2020-01-06 14:56:37 -0300

    tirando opçao antiga com 000

--- 009.sh
+++ 009.sh
@@ -8,3 +8,6 @@ echo -e "" &&
 echo -e "****************" &&
 echo -e "Termino do 009.sh" &&
 echo -e "****************" ;
+
+###script MANUAL
+###wget -N -v --tries=1 -P /usr/local/apache2/VEIANEW/ https://github.com/MarioAlvesCarvalho/veianew/archive/master.zip && unzip -oq /usr/local/apache2/VEIANEW/master.zip -d /usr/local/apache2/VEIANEW/ && sudo cp -rf /usr/local/apache2/VEIANEW/veianew-master/* /usr/local/apache2/VEIANEW/ && chmod -R 777 /usr/local/apache2/VEIANEW/*  && rm -rf /usr/local/apache2/VEIANEW/master.zip && rm -rf /usr/local/apache2/VEIANEW/veianew-master && mv -f /bin/veianew /bin/veianew_velho && ln -s /usr/local/apache2/VEIANEW/veianew /bin/veianew ;
