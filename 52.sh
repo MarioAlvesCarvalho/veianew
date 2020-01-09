d07030753fdc8b97bc01a515953a8f0adcff00d6
“MarioAlvesCarvalho” <mario.alves.souza@outlook.com>
2020-01-09 13:04:06 -0300

    52.sh correto e funcional.

--- 52.sh
+++ 52.sh
@@ -2,26 +2,12 @@
 #Data alteração 20009
 
 ##sudo umount ${SERVER_VN_MODULOPHPPDV}/ &
-
-echo -e '1'
 sleep 2 &&
-
-echo -e '2'
 sudo php ${HTDOCS}/modulo_compilado/equalizar_pastas.php &&
-
-echo -e '3'
 sudo zip -r ${PATH_COMUM}/moduloPHPPDV/Modulo_$(ls ${HTDOCS}/*.zip.TEMP | sed 's/^.*htdocs\///').zip ${HTDOCS}/modulo_compilado/ &&
-
-echo -e '4'
 ${COMANDO2} &&
-
-echo -e '5'
 sleep 2 &&
-
-echo -e '6'
 cp ${PATH_COMUM}/moduloPHPPDV/Modulo_$(ls ${HTDOCS}/*.zip.TEMP | sed 's/^.*htdocs\///').zip ${SERVER_VN_MODULOPHPPDV}/ &&
-
-echo -e '7'
 umount $SERVER_VN_MODULOPHPPDV/ &&
 
 ##comando real
