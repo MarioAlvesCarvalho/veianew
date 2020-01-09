b8d0429016d86e0fc874b665b7bb17236195b9cc
“MarioAlvesCarvalho” <mario.alves.souza@outlook.com>
2020-01-09 14:01:24 -0300

    acertando BKP e BKPMODULO. v5.0.34_BETA

--- BKP.sh
+++ BKP.sh
@@ -1,22 +1,40 @@
 #!/bin/bash
-#Data alteração 20005
+#Data alteração 20009
 
-$COMANDO1 &&
-$COMANDO3 &&
-sleep 3 &&
-rsync -uv $PATH_015_SCRIPTS_VEIANEW/BKP_manager.sh $manager/BKP_manager.sh &&
-chmod -x $manager/BKP_manager.sh &&
-$manager/BKP_manager.sh &&
-sleep 5 &&
-rm $manager/BKP_manager.sh &&
+$COMANDO1 &
+$COMANDO3 &
 
-#if umount MANAGER
-#   umount $manager/  &&
-sleep 2 &&
+for ARQ_BKP in `find ${DIR_BKP}/*.zip -ctime +4 -exec ls -lt {} \; | awk '{print $9}' `; do
+ANOMES_ARQ_BKP=$(ls --full-time arq | awk '{print $6}' | cut -c1-7)
+
+if [ "${ANOMES}" = "${ANOMES_ARQ_BKP}" ]; then
+mkdir -p ${DESTINO_BKP}/${ANOMES}
+mv -f -v ${ARQ_BKP} ${DESTINO_BKP}/${ANOMES}
+
+else
+if [ "${ANOMES_1}" = "${ANOMES_ARQ_BKP}" ]; then
+mkdir -p ${DESTINO_BKP}/${ANOMES_1}
+mv -f -v ${ARQ_BKP} ${DESTINO_BKP}/$ANOMES_1
+
+else
+if [ "${ANOMES_2}" = "${ANOMES_ARQ_BKP}" ]; then
+mkdir -p ${DESTINO_BKP}/${ANOMES_2}
+mv -f -v ${ARQ_BKP} ${DESTINO_BKP}/${ANOMES_2}
+
+else
+if [ "${ANOMES_3}" = "${ANOMES_ARQ_BKP}" ]; then
+mkdir -p ${DESTINO_BKP}/${ANOMES_3}
+mv -f -v ${ARQ_BKP} ${DESTINO_BKP}/${ANOMES_3}
+
+fi
+fi
+fi
+fi
+done
 echo -e "" &&
 echo -e "" &&
 echo -e "" &&
-echo -e "****************" &&
+echo -e "*****************" &&
 echo -e "Termino do BKP.sh" &&
-echo -e "****************" &&
+echo -e "*****************" &&
 veianew ;
--- BKPMODULO.sh
+++ BKPMODULO.sh
@@ -1,20 +1,40 @@
 #!/bin/bash
-#Data alteração 20005
+#Data alteração 20009
 
-${COMANDO1} &&
-${COMANDO3} &&
-sleep 3 &&
-rsync -uv ${SERVER_VN_SCRIPTS_VEIANEW}/BKP_moduloPHPPDV.sh ${SERVER_VN_MODULOPHPPDV}/BKP_moduloPHPPDV.sh &&
-chmod -x ${SERVER_VN_MODULOPHPPDV}/BKP_moduloPHPPDV.sh &&
-${SERVER_VN_MODULOPHPPDV}/BKP_moduloPHPPDV.sh &&
-sleep 5 &&
-rm ${SERVER_VN_MODULOPHPPDV}/BKP_moduloPHPPDV.sh &&
-umount ${SERVER_VN_MODULOPHPPDV}/ &&
-sleep 2 &&
+$COMANDO1 &
+$COMANDO3 &
+
+for ARQ_BKPMODULO in `find ${DIR_BKPMODULO}/*.zip -ctime +4 -exec ls -lt {} \; | awk '{print $9}' `; do
+ANOMES_ARQ_BKPMODULO=$(ls --full-time arq | awk '{print $6}' | cut -c1-7)
+
+if [ "${ANOMES}" = "${ANOMES_ARQ_BKPMODULO}" ]; then
+mkdir -p ${DESTINO_BKPMODULO}/${ANOMES}
+mv -f -v ${ARQ_BKPMODULO} ${DESTINO_BKPMODULO}/${ANOMES}
+
+else
+if [ "${ANOMES_1}" = "${ANOMES_ARQ_BKPMODULO}" ]; then
+mkdir -p ${DESTINO_BKPMODULO}/${ANOMES_1}
+mv -f -v ${ARQ_BKPMODULO} ${DESTINO_BKPMODULO}/$ANOMES_1
+
+else
+if [ "${ANOMES_2}" = "${ANOMES_ARQ_BKPMODULO}" ]; then
+mkdir -p ${DESTINO_BKPMODULO}/${ANOMES_2}
+mv -f -v ${ARQ_BKPMODULO} ${DESTINO_BKPMODULO}/${ANOMES_2}
+
+else
+if [ "${ANOMES_3}" = "${ANOMES_ARQ_BKPMODULO}" ]; then
+mkdir -p ${DESTINO_BKPMODULO}/${ANOMES_3}
+mv -f -v ${ARQ_BKPMODULO} ${DESTINO_BKPMODULO}/${ANOMES_3}
+
+fi
+fi
+fi
+fi
+done
 echo -e "" &&
 echo -e "" &&
 echo -e "" &&
-echo -e "****************" &&
+echo -e "***********************" &&
 echo -e "Termino do BKPMODULO.sh" &&
-echo -e "****************" &&
+echo -e "***********************" &&
 veianew ;
