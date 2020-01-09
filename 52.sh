ed957284eede508463713583a7d0275507d073a8
“MarioAlvesCarvalho” <mario.alves.souza@outlook.com>
2020-01-09 10:51:36 -0300

    Acertando 52.sh v5.0.33_BETA

--- 52.sh
+++ 52.sh
@@ -1,34 +1,20 @@
 #!/bin/bash
-#Data alteração 20006
+#Data alteração 20009
 
-echo -e '0'
-#sudo umount ${SERVER_VN_MODULOPHPPDV}/ &&
-
-echo -e '1'
+sudo umount ${SERVER_VN_MODULOPHPPDV}/ &&
 sleep 2 &&
-
-echo -e '2'
 sudo php ${HTDOCS}/modulo_compilado/equalizar_pastas.php &&
-
-echo -e '3'
-sudo zip -r ${HTDOCS}/modulo_compilado ${HTDOCS}/Modulo_$(ls ${HTDOCS}/*.zip.TEMP).zip * &&
-
-echo -e '4'
+sudo zip -r ${PATH_COMUM}/moduloPHPPDV/Modulo_$(ls ${HTDOCS}/*.zip.TEMP | sed 's/^.*htdocs\///').zip ${HTDOCS}/modulo_compilado/ &&
 ${COMANDO2} &&
-
-echo -e '5'
 sleep 2 &&
-
-echo -e '6'
-cp Modulo_$(ls ${HTDOCS}/*.zip.TEMP).zip ${SERVER_VN_MODULOPHPPDV}/ &&
-
-echo -e '7'
+cp ${PATH_COMUM}/moduloPHPPDV/Modulo_$(ls ${HTDOCS}/*.zip.TEMP | sed 's/^.*htdocs\///').zip ${SERVER_VN_MODULOPHPPDV}/ &&
 umount $SERVER_VN_MODULOPHPPDV/ &&
 
-echo -e '8'
-sleep 1 &&
+##comando real
+##sudo zip -r /Zanthus/Zeus/path_comum/moduloPHPPDV/Modulo_$(ls /usr/local/apache22/htdocs/*.zip.TEMP | sed 's/^.*htdocs\///').zip /usr/local/apache22/htdocs/modulo_compilado/ &&
+##cp /Zanthus/Zeus/path_comum/moduloPHPPDV/Modulo_$(ls /usr/local/apache22/htdocs/*.zip.TEMP | sed 's/^.*htdocs\///').zip /Zanthus/Zeus/path_comum/0.66/MODULOPHPPDV/ &&
 
-echo -e '9 fim'
+sleep 1 &&
 echo -e "" &&
 echo -e "" &&
 echo -e "" &&
