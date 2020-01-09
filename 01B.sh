abdeb0b5b26f48069aaa9029ad625642cd58dc7d
“MarioAlvesCarvalho” <mario.alves.souza@outlook.com>
2020-01-09 14:57:46 -0300

    01B

--- 01B.sh
+++ 01B.sh
@@ -2,13 +2,13 @@
 #Data alteração 20009
 
 wget -N -v --tries=1 -P $VEIANEW https://github.com/MarioAlvesCarvalho/veianew/archive/BETA.zip &&
-#chmod -R 777  $VEIANEW/BETA.zip &&
-#chmod +x $VEIANEW/BETA.zip &&
-#unzip -oq $VEIANEW/BETA.zip -d $VEIANEW &&
-#sudo cp -rf $VEIANEW/veianew-BETA/* $VEIANEW/ &&
-#sudo rm -rf $VEIANEW/BETA.zip &&
-#sudo rm -rf $VEIANEW/veianew-BETA &&
-#sudo chmod 775 $VEIANEW/*.sh &&
+chmod -R 777  $VEIANEW/BETA.zip &&
+chmod +x $VEIANEW/BETA.zip &&
+unzip -oq $VEIANEW/BETA.zip -d $VEIANEW &&
+sudo cp -rf $VEIANEW/veianew-BETA/* $VEIANEW/ &&
+sudo rm -rf $VEIANEW/BETA.zip &&
+sudo rm -rf $VEIANEW/veianew-BETA &&
+sudo chmod 775 $VEIANEW/*.sh &&
 echo -e "" &&
 echo -e "" &&
 echo -e "" &&
