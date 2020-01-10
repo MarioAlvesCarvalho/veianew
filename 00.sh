49a9baa4e6b4f188a987501dce69f9a0f2a2f149
“MarioAlvesCarvalho” <mario.alves.souza@outlook.com>
2020-01-10 15:10:59 -0300

    REV. 5

--- 00.sh
+++ 00.sh
@@ -1,21 +1,25 @@
 
 #!/bin/bash
 #Data alteração 20010
-#REV. 4
+#REV. 5
 
 mountpoint -q ${SERVER_VN_MODULOPHPPDV}
 if [ $? == 0 ]
 then
-    echo 'esta montado, vamos desmontar'
     umount ${SERVER_VN_MODULOPHPPDV}
-else
-    echo 'NAO montado, vamos sair'
 fi
 
-#${SERVER_VN_MODULOPHPPDV}
-#${SERVER_VN_SCRIPTS_VEIANEW}
-#${SERVER_VN_MODULO_COMPILADO}
+mountpoint -q ${SERVER_VN_SCRIPTS_VEIANEW}
+if [ $? == 0 ]
+then
+    umount ${SERVER_VN_SCRIPTS_VEIANEW}
+fi
 
+mountpoint -q ${SERVER_VN_MODULO_COMPILADO}
+if [ $? == 0 ]
+then
+    umount ${SERVER_VN_MODULO_COMPILADO}
+fi
 
 echo -e "" &&
 echo -e "" &&
