f5daba8bbaf1e2f9990ddd5dda3b1950140b04cf
“MarioAlvesCarvalho” <mario.alves.souza@outlook.com>
2020-01-10 14:07:48 -0300

    (mountpoint -q

--- 00.sh
+++ 00.sh
@@ -20,7 +20,7 @@ echo 'inicio 1'
 i='0'
 while [ $i = '0' ]; do
   echo 'antes mountpoint'
-  if [$(mountpoint  ${SERVER_VN_MODULOPHPPDV}) | $? == 0 ]
+  if [$(mountpoint -q ${SERVER_VN_MODULOPHPPDV}) | $? == 0 ]
   then
     echo 'esta montado, vamos desmontar. '
     umount ${SERVER_VN_MODULOPHPPDV}
