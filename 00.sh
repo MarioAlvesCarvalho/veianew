3b6c957623c023400ce60a3c59e13b99af550e6b
“MarioAlvesCarvalho” <mario.alves.souza@outlook.com>
2020-01-10 08:46:06 -0300

    rev1

--- 00.sh
+++ 00.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 #Data alteração 20010
+#REV. 1
 
+
+if [ 'mount | grep 192.168.0.66' -ne "0" ]
+then
 umount ${SERVER_VN}/* &&
+  do
 
 
-exit &
+
+#exit &
 echo -e "" &&
 echo -e "" &&
 echo -e "" &&
