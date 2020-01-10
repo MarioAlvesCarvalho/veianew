b4471b4e268602919bf2f4c044f29084484a2114
“MarioAlvesCarvalho” <mario.alves.souza@outlook.com>
2020-01-10 13:06:24 -0300

    REV. 2

--- 00.sh
+++ 00.sh
@@ -3,14 +3,17 @@
 #Data alteração 20010
 #REV. 1
 
+MOUNTPOINT_SERVER_VN_MODULOPHPPDV(){
 mountpoint -q ${SERVER_VN_MODULOPHPPDV}
 if [ $? == 0 ]
   then
     echo 'esta montado, vamos desmontar. '
     umount ${SERVER_VN_MODULOPHPPDV}
-  else
+  }
+  MOUNTPOINT_SERVER_VN_MODULOPHPPDV
+else
+  echo'.'
 fi
-
 
 
 #${SERVER_VN_MODULOPHPPDV}
