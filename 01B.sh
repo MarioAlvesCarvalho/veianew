85267c77aa3f765dcd0abc95b2ab287cc5bd1e1e
“MarioAlvesCarvalho” <mario.alves.souza@outlook.com>
2020-01-13 09:35:37 -0300

    sincronizando master e beta

--- 01B.sh
+++ 01B.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
-#Data alteração 20009
+#Data alteração 20010
+#REV. 1
 
-wget -N -v --tries=1 -P $VEIANEW https://github.com/MarioAlvesCarvalho/veianew/archive/BETA.zip &&
-chmod -R 777  $VEIANEW/BETA.zip &&
-chmod +x $VEIANEW/BETA.zip &&
-unzip -oq $VEIANEW/BETA.zip -d $VEIANEW &&
-sudo cp -rf $VEIANEW/veianew-BETA/* $VEIANEW/ &&
-sudo rm -rf $VEIANEW/BETA.zip &&
-sudo rm -rf $VEIANEW/veianew-BETA &&
-sudo chmod 775 $VEIANEW/*.sh &&
-echo -e "" &&
-echo -e "" &&
-echo -e "" &&
-echo -e 'Atualização do veianew BETA concluida com sucesso!!!' &&
-echo -e "" &&
-echo -e "*****************" &&
-echo -e "Termino do 001.sh" &&
+wget -N -v --tries=1 -P $VEIANEW https://github.com/MarioAlvesCarvalho/veianew/archive/BETA.zip
+chmod -R 777  $VEIANEW/BETA.zip
+chmod +x $VEIANEW/BETA.zip
+rm -rf $VEIANEW/*.sh
+unzip -oq $VEIANEW/BETA.zip -d $VEIANEW
+sudo cp -rf $VEIANEW/veianew-BETA/* $VEIANEW/
+sudo rm -rf $VEIANEW/BETA.zip
+sudo rm -rf $VEIANEW/veianew-BETA
+sudo chmod 775 $VEIANEW/*.sh
+echo -e ""
+echo -e ""
+echo -e ""
+echo -e 'Atualização do veianew BETA concluida com sucesso!!!'
+echo -e ""
+echo -e "*****************"
+echo -e "Termino do 01B.sh"
 echo -e "*****************" ;
