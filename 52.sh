3e6c8089cb54ca42fa78c9caaf63ab5590e1e89e
“MarioAlvesCarvalho” <mario.alves.souza@outlook.com>
2020-01-13 09:01:39 -0300

    padrao debug

--- 52.sh
+++ 52.sh
@@ -2,28 +2,28 @@
 #Data alteração 20009
 
 ##sudo umount ${SERVER_VN_MODULOPHPPDV}/ &
-echo '1'
+echo '.'
 sleep 2 &&
 
-echo '2'
+echo '..'
 sudo php ${HTDOCS}/modulo_compilado/equalizar_pastas.php &&
 
-echo '3'
+echo '...'
 sudo zip -r ${PATH_COMUM}/moduloPHPPDV/Modulo_$(ls ${HTDOCS}/*.zip.TEMP | sed 's/^.*htdocs\///').zip ${HTDOCS}/modulo_compilado/ &&
 
-echo '4'
+echo '... .'
 ${COMANDO2} &&
 
-echo '5'
+echo '... ..'
 sleep 2 &&
 
-echo '6'
+echo '... ...'
 cp ${PATH_COMUM}/moduloPHPPDV/Modulo_$(ls ${HTDOCS}/*.zip.TEMP | sed 's/^.*htdocs\///').zip ${SERVER_VN_MODULOPHPPDV}/ &&
 
-echo '7'
+echo '... ... .'
 umount $SERVER_VN_MODULOPHPPDV/ &&
 
-echo 'fim'
+echo '.   .'
 ##comando real
 ##sudo zip -r /Zanthus/Zeus/path_comum/moduloPHPPDV/Modulo_$(ls /usr/local/apache22/htdocs/*.zip.TEMP | sed 's/^.*htdocs\///').zip /usr/local/apache22/htdocs/modulo_compilado/ &&
 ##cp /Zanthus/Zeus/path_comum/moduloPHPPDV/Modulo_$(ls /usr/local/apache22/htdocs/*.zip.TEMP | sed 's/^.*htdocs\///').zip /Zanthus/Zeus/path_comum/0.66/MODULOPHPPDV/ &&
