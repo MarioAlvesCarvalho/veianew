3f2d0bdb27b498cccc0dd5ab33cb1fff1d6c9a7e
“MarioAlvesCarvalho” <mario.alves.souza@outlook.com>
2020-01-13 09:30:44 -0300

    sincronizando master e beta

--- 00.sh
+++ 00.sh
@@ -1,8 +1,26 @@
-#!/bin/bash
-#Data alteração 20005
 
-umount ${SERVER_VN}/* &&
-exit &
+#!/bin/bash
+#Data alteração 20010
+#REV. 5
+
+mountpoint -q ${SERVER_VN_MODULOPHPPDV}
+if [ $? == 0 ]
+then
+    umount ${SERVER_VN_MODULOPHPPDV}
+fi
+
+mountpoint -q ${SERVER_VN_SCRIPTS_VEIANEW}
+if [ $? == 0 ]
+then
+    umount ${SERVER_VN_SCRIPTS_VEIANEW}
+fi
+
+mountpoint -q ${SERVER_VN_MODULO_COMPILADO}
+if [ $? == 0 ]
+then
+    umount ${SERVER_VN_MODULO_COMPILADO}
+fi
+
 echo -e "" &&
 echo -e "" &&
 echo -e "" &&
