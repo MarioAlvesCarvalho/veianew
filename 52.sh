3edac5b40a37f8744646986dea4d33046dd86017
“MarioAlvesCarvalho” <mario.alves.souza@outlook.com>
2020-01-13 09:32:05 -0300

    sincronizando master e beta

--- 52.sh
+++ 52.sh
@@ -2,14 +2,53 @@
 #Data alteração 20009
 
 ##sudo umount ${SERVER_VN_MODULOPHPPDV}/ &
+echo '.'
 sleep 2 &&
+
+echo '..'
 sudo php ${HTDOCS}/modulo_compilado/equalizar_pastas.php &&
+
+echo '...'
 sudo zip -r ${PATH_COMUM}/moduloPHPPDV/Modulo_$(ls ${HTDOCS}/*.zip.TEMP | sed 's/^.*htdocs\///').zip ${HTDOCS}/modulo_compilado/ &&
+
+echo '... .'
+umount $SERVER_VN_MODULOPHPPDV/
+umount $SERVER_VN_MODULOPHPPDV/
+umount $SERVER_VN_MODULOPHPPDV/
+umount $SERVER_VN_MODULOPHPPDV/
 ${COMANDO2} &&
+
+echo '... ..'
 sleep 2 &&
+
+echo '... ...'
 cp ${PATH_COMUM}/moduloPHPPDV/Modulo_$(ls ${HTDOCS}/*.zip.TEMP | sed 's/^.*htdocs\///').zip ${SERVER_VN_MODULOPHPPDV}/ &&
+
+# renomeando no SERVER_VN_MODULOPHPPDV de .zip.TEMP.zip para .zip
+echo '... ... .'
+cd  ${SERVER_VN_MODULOPHPPDV}
+for arquivo in *.zip.TEMP.zip
+do
+newname=$(basename ${arquivo} zip.TEMP.zip)zip
+mv ${arquivo} ${newname}
+done
+
+# renomeando no PATH_COMUM de .zip.TEMP.zip para .zip
+echo '... ... ..'
+cd  ${PATH_COMUM}/moduloPHPPDV/
+for arquivo in *.zip.TEMP.zip
+do
+newname=$(basename ${arquivo} zip.TEMP.zip)zip
+mv ${arquivo} ${newname}
+done
+
+echo '... ... ...'
+sleep 1 &&
+
+echo '... ... ... .'
 umount $SERVER_VN_MODULOPHPPDV/ &&
 
+echo '.   .'
 ##comando real
 ##sudo zip -r /Zanthus/Zeus/path_comum/moduloPHPPDV/Modulo_$(ls /usr/local/apache22/htdocs/*.zip.TEMP | sed 's/^.*htdocs\///').zip /usr/local/apache22/htdocs/modulo_compilado/ &&
 ##cp /Zanthus/Zeus/path_comum/moduloPHPPDV/Modulo_$(ls /usr/local/apache22/htdocs/*.zip.TEMP | sed 's/^.*htdocs\///').zip /Zanthus/Zeus/path_comum/0.66/MODULOPHPPDV/ &&
