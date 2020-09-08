9fee95ca21aabdbabd6ecf62658af316ac73c5a7
MarioAlvesCarvalho <github113288>
2020-09-08 09:14:11 -0300

    v5.6.1

--- 99.sh
+++ 99.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #Data alteração 20249
-sleep 3 &
-echo -e "INICIO DO 99.sh" &
-${APACHE2}/bin/apachectl start &
-mkdir ${MANAGER}/Logs &
-chown -R zanthus.zanthus ${MANAGER}/Logs/ &
-sleep 1 &
+sleep 3 &&
+echo -e "INICIO DO 99.sh" &&
+${APACHE2}/bin/apachectl start &&
+mkdir ${MANAGER}/Logs &&
+chown -R zanthus.zanthus ${MANAGER}/Logs/ &&
+sleep 1 &&
 echo -e "" &&
 echo -e "" &&
 echo -e "" &&
