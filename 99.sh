f0a31fc280656159019a3b3f7f9e0f1e75243296
MarioAlvesCarvalho <github113288>
2020-09-09 16:40:18 -0300

    V5.6.3 20253

--- 99.sh
+++ 99.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#Data alteração 20252
+#Data alteração 20253
 
-${APACHE2}/bin/apachectl start &&
-mkdir ${MANAGER}/Logs &&
-chown -R zanthus.zanthus ${MANAGER}/Logs/ &&
+${APACHE2}/bin/apachectl start &
+mkdir ${MANAGER}/Logs &
+chown -R zanthus.zanthus ${MANAGER}/Logs/ &
 sleep 1 &&
 echo -e "" &&
 echo -e "" &&
