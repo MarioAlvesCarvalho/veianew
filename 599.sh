291e4701fd5c07c390fa8cc50f0b695bc4ad2d9f
MarioAlvesCarvalho <github113288>
2020-11-17 20:05:47 -0300

    Acerto final 599.sh OK

--- 599.sh
+++ 599.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
-#Data alteração 20252
+#Data alteração 20322
 
-rm -rf ${HTDOCS}/php_*.zip &&
+unset ARQ_BKP
+
+for ARQ_BKP in `find ${HTDOCS} -maxdepth 1  -name '*anager*.zip' -mtime +20 -exec ls -lt {} \; | awk '{print $9}' `
+do
+echo -e "Excluindo pacotes do Manager com mais de 20 dias!!!"
+if [ -f ${ARQ_BKP}  ] ; then
+mv -f  ${ARQ_BKP}  ${HTDOCS}/EXCLUIDO/
+else
+echo -e "Não há pacotes do Manager com mais de 20 dias !!!"
+fi
+done
+
 sleep 1 &&
 echo -e "" &&
 echo -e "" &&
 echo -e "" &&
-echo -e "****************" &&
+echo -e "***********************" &&
 echo -e "Termino do 599.sh" &&
-echo -e "****************" &&
-veianew ;
+echo -e "***********************" &&
+veianew
