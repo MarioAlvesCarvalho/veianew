7bfd91fb31ac359370c96e6daa46767bc5e50e34
MarioAlvesCarvalho <github113288>
2020-11-17 19:59:08 -0300

    "${ARQ_BKP}" foi gerado a mais de 20 e sera excluido

--- BKP.sh
+++ BKP.sh
@@ -6,6 +6,8 @@ unset ARQ_BKP
 
 for ARQ_BKP in `find ${HTDOCS} -maxdepth 1  -name '*anager*.zip' -mtime +20 -exec ls -lt {} \; | awk '{print $9}' `
 do
+echo -e "Excluindo pacotes do manager com mais de 20 dias!!!"
+
 if [ -f ${ARQ_BKP}  ] ; then
 mv -f  ${ARQ_BKP}  ${HTDOCS}/EXCLUIDO/
 echo -e "Pacote "${ARQ_BKP}" foi gerado a mais de 20 e sera excluido !!!"
