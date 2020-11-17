c22ccdecd7d811aab206e7bb4d324d610ed72d91
MarioAlvesCarvalho <github113288>
2020-11-17 20:00:59 -0300

    teste

--- BKP.sh
+++ BKP.sh
@@ -5,12 +5,12 @@ echo -e "TESTE_BKP25"
 unset ARQ_BKP
 
 for ARQ_BKP in `find ${HTDOCS} -maxdepth 1  -name '*anager*.zip' -mtime +20 -exec ls -lt {} \; | awk '{print $9}' `
+echo -e "***Não há pacotes do Manager com mais de 20 dias !!!"
 do
 echo -e "Excluindo pacotes do manager com mais de 20 dias!!!"
 
 if [ -f ${ARQ_BKP}  ] ; then
 mv -f  ${ARQ_BKP}  ${HTDOCS}/EXCLUIDO/
-echo -e "Pacote "${ARQ_BKP}" foi gerado a mais de 20 e sera excluido !!!"
 else
 echo -e "Não há pacotes do Manager com mais de 20 dias !!!"
 fi
@@ -22,4 +22,4 @@ echo -e "" &&
 echo -e "***********************" &&
 echo -e "Termino do BKP.sh" &&
 echo -e "***********************" &&
-echo -e "***********************"
+veianew
