2d52159dc4ac34fbb3d4be5f0a94c3431ca832e6
MarioAlvesCarvalho <github113288>
2020-11-17 20:05:35 -0300

    **** DESATIVADO ****

--- BKP.sh
+++ BKP.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 #Data alteração 20322
 
-echo -e "TESTE_BKP25"
 unset ARQ_BKP
 
 for ARQ_BKP in `find ${HTDOCS} -maxdepth 1  -name '*anager*.zip' -mtime +20 -exec ls -lt {} \; | awk '{print $9}' `
-echo -e "***Não há pacotes do Manager com mais de 20 dias !!!"
 do
-echo -e "Excluindo pacotes do manager com mais de 20 dias!!!"
-
+echo -e "Excluindo pacotes do Manager com mais de 20 dias!!!"
 if [ -f ${ARQ_BKP}  ] ; then
 mv -f  ${ARQ_BKP}  ${HTDOCS}/EXCLUIDO/
 else
@@ -20,6 +17,6 @@ echo -e "" &&
 echo -e "" &&
 echo -e "" &&
 echo -e "***********************" &&
-echo -e "Termino do BKP.sh" &&
+echo -e "Termino do 599.sh" &&
 echo -e "***********************" &&
 veianew
