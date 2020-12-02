c865caffc8fd51adf6a01e46b97709266db416e4
“MarioAlvesCarvalho” <mario.alves.souza@outlook.com>
2020-12-02 09:16:24 -0300

    "#REV. 6"

--- 00.sh
+++ 00.sh
@@ -1,25 +1,27 @@
 
 #!/bin/bash
-#Data alteração 20010
-echo -e "#REV. 5"
+#Data alteração 20337
+echo -e "#REV. 6"
 
-mountpoint -q ${SERVER_VN_MODULOPHPPDV}
-if [ $? == 0 ]
-then
-    umount ${SERVER_VN_MODULOPHPPDV}
-fi
+#mountpoint -q ${SERVER_VN_MODULOPHPPDV}
+#if [ $? == 0 ]
+#then
+#    umount ${SERVER_VN_MODULOPHPPDV}
+#fi
 
-mountpoint -q ${SERVER_VN_SCRIPTS_VEIANEW}
-if [ $? == 0 ]
-then
-    umount ${SERVER_VN_SCRIPTS_VEIANEW}
-fi
+#mountpoint -q ${SERVER_VN_SCRIPTS_VEIANEW}
+#if [ $? == 0 ]
+#then
+#    umount ${SERVER_VN_SCRIPTS_VEIANEW}
+#fi
 
-mountpoint -q ${SERVER_VN_MODULO_COMPILADO}
-if [ $? == 0 ]
-then
-    umount ${SERVER_VN_MODULO_COMPILADO}
-fi
+#mountpoint -q ${SERVER_VN_MODULO_COMPILADO}
+#if [ $? == 0 ]
+#then
+#    umount ${SERVER_VN_MODULO_COMPILADO}
+#fi
+
+echo -e "Fechando veianew" &&
 
 echo -e "" &&
 echo -e "" &&
