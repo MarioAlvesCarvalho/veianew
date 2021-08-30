acbd6df8fe534cc56c3e7e99944ff1a056f3a9c7
MarioAlvesCarvalho <mario.alves.souza@outlook.com>
2021-08-30 08:52:22 -0300

    retirando reset. criando opção 52

--- 52.sh
+++ 52.sh
@@ -1,66 +1,8 @@
 #!/bin/bash
-#Data alteração 20322
+#Data alteração 21242
 #REV. 2
 
-#### DESATIVADO ####
-
-echo -e "#REV. 1"
-
-##sudo umount ${SERVER_VN_MODULOPHPPDV}/ &
-echo '.'
-sleep 2 &&
-
-echo '..'
-cd ${HTDOCS}/modulo_compilado/
-sudo php equalizar_pastas.php &&
-
-echo '...'
-cd ${HTDOCS}/modulo_compilado/
-sudo zip -r ${PATH_COMUM}/moduloPHPPDV/Modulo_$(ls ${HTDOCS}/*.zip.TEMP | sed 's/^.*htdocs\///').zip * &&
-
-echo '... .'
-umount $SERVER_VN_MODULOPHPPDV/
-umount $SERVER_VN_MODULOPHPPDV/
-umount $SERVER_VN_MODULOPHPPDV/
-umount $SERVER_VN_MODULOPHPPDV/
-${COMANDO2} &&
-
-echo '... ..'
-sleep 2 &&
-
-echo '... ...'
-cp ${PATH_COMUM}/moduloPHPPDV/Modulo_$(ls ${HTDOCS}/*.zip.TEMP | sed 's/^.*htdocs\///').zip ${SERVER_VN_MODULOPHPPDV}/ &&
-
-# renomeando no SERVER_VN_MODULOPHPPDV de .zip.TEMP.zip para .zip
-echo '... ... .'
-cd  ${SERVER_VN_MODULOPHPPDV}
-for arquivo in *.zip.TEMP.zip
-do
-newname=$(basename ${arquivo} zip.TEMP.zip)zip
-mv ${arquivo} ${newname}
-done
-
-# renomeando no PATH_COMUM de .zip.TEMP.zip para .zip
-echo '... ... ..'
-cd  ${PATH_COMUM}/moduloPHPPDV/
-for arquivo in *.zip.TEMP.zip
-do
-newname=$(basename ${arquivo} zip.TEMP.zip)zip
-mv ${arquivo} ${newname}
-done
-
-echo '... ... ...'
-sleep 1 &&
-
-echo '... ... ... .'
-umount $SERVER_VN_MODULOPHPPDV/ &&
-
-echo '.   .'
-##comando real
-##sudo zip -r /Zanthus/Zeus/path_comum/moduloPHPPDV/Modulo_$(ls /usr/local/apache22/htdocs/*.zip.TEMP | sed 's/^.*htdocs\///').zip /usr/local/apache22/htdocs/modulo_compilado/ &&
-##cp /Zanthus/Zeus/path_comum/moduloPHPPDV/Modulo_$(ls /usr/local/apache22/htdocs/*.zip.TEMP | sed 's/^.*htdocs\///').zip /Zanthus/Zeus/path_comum/0.66/MODULOPHPPDV/ &&
-
-sleep 1 &&
+sudo find ${MANAGER}/Kernel -iname '*' -exec rm -rf {} \; &
 echo -e "" &&
 echo -e "" &&
 echo -e "" &&
