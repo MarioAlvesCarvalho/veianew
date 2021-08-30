de5a3fdf21bfa8be3552fbfed086c980d0ebfaed
MarioAlvesCarvalho <mario.alves.souza@outlook.com>
2021-08-30 11:04:23 -0300

    #REV. 2

--- 52.sh
+++ 52.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 #Data alteração 21242
-#REV. 2
+#REV. 3
 
-sudo find ${MANAGER}/Kernel -iname '*' -exec rm -rf {} \; &
+sudo find ${MANAGER}/Kernel/ -iname 'TH*.TR*' -exec rm -rf {} \; &
+
 echo -e "" &&
 echo -e "" &&
 echo -e "" &&
