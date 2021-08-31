aee282f319d624b8bcb60538499d831146cf13f0
MarioAlvesCarvalho <mario.alves.souza@outlook.com>
2021-08-31 10:54:30 -0300

    9.0.4

--- 06.sh
+++ 06.sh
@@ -9,6 +9,8 @@ sleep 3 &&
 
 if [ -d "${PATH_COMUM}/so" ] ; then
 echo -e "Validando ${PATH_COMUM}/so ..."
+chmod 777 "${PATH_COMUM}/so"
+chown root.root "${PATH_COMUM}/so"
 else
 echo -e "Criando diretorio ${PATH_COMUM}/so"
 mkdir "${PATH_COMUM}/so"
@@ -18,6 +20,8 @@ fi
 
 if [ -d "${PATH_COMUM}/so_co5" ] ; then
 echo -e "Validando ${PATH_COMUM}/so_co5 ..."
+chmod 777 "${PATH_COMUM}/so_co5"
+chown root.root "${PATH_COMUM}/so_co5"
 else
 echo -e "Criando diretorio ${PATH_COMUM}/so_co5"
 mkdir "${PATH_COMUM}/so_co5"
@@ -27,6 +31,8 @@ fi
 
 if [ -d "${PATH_COMUM}/so_r64" ] ; then
 echo -e "Validando ${PATH_COMUM}/so_r64 ..."
+chmod 777 "${PATH_COMUM}/so_r64"
+chown root.root "${PATH_COMUM}/so_r64"
 else
 echo -e "Criando diretorio ${PATH_COMUM}/so_r64"
 mkdir "${PATH_COMUM}/so_r64"
@@ -36,6 +42,8 @@ fi
 
 if [ -d "${PATH_COMUM}/so_u64" ] ; then
 echo -e "Validando ${PATH_COMUM}/so_u64 ..."
+chmod 777 "${PATH_COMUM}/so_u64"
+chown root.root "${PATH_COMUM}/so_u64"
 else
 echo -e "Criando diretorio ${PATH_COMUM}/so_u64"
 mkdir "${PATH_COMUM}/so_u64"
@@ -45,6 +53,8 @@ fi
 
 if [ -d "${PATH_COMUM}/so_ubu" ] ; then
 echo -e "Validando ${PATH_COMUM}/so_ubu ..."
+chmod 777 "${PATH_COMUM}/so_ubu"
+chown root.root "${PATH_COMUM}/so_ubu"
 else
 echo -e "Criando diretorio ${PATH_COMUM}/so_ubu"
 mkdir "${PATH_COMUM}/so_ubu"
@@ -54,10 +64,12 @@ fi
 
 if [ -d "${PATH_COMUM}/dlls" ] ; then
 echo -e "Validando ${PATH_COMUM}/dlls ..."
+chmod 777 "${PATH_COMUM}/dlls" 
+chown root.root "${PATH_COMUM}/dlls"
 else
 echo -e "Criando diretorio ${PATH_COMUM}/dlls"
 mkdir "${PATH_COMUM}/dlls"
-chmod 777 "${PATH_COMUM}/dlls"
+chmod 777 "${PATH_COMUM}/dlls" 
 chown root.root "${PATH_COMUM}/dlls"
 fi
 
@@ -67,11 +79,6 @@ wget -N --tries=2 -P ${PATH_COMUM}/so_r64 ${FTP_COMPLEMENTARES}/so_r64/*  &&
 wget -N --tries=2 -P ${PATH_COMUM}/so_u64 ${FTP_COMPLEMENTARES}/so_u64/*  &&
 wget -N --tries=2 -P ${PATH_COMUM}/so_ubu ${FTP_COMPLEMENTARES}/so_ubu/*  &&
 wget -N --tries=2 -P ${PATH_COMUM}/dlls   ${FTP_COMPLEMENTARES}/dlls/*    &&
-
-
-
-
-
 
 sleep 1 &&
 echo -e "" &&
