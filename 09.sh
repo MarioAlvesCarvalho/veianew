cfeb1d9bd6f0af6d4124e4a2f2b66b17b10010e2
MarioAlvesCarvalho <mario.alves.souza@outlook.com>
2021-08-31 17:05:23 -0300

    "v9.1"

--- 09.sh
+++ 09.sh
@@ -3,16 +3,18 @@
 
 echo -e "#REV. 1"
 
-#echo -e 'Essa opção não está ativa!!!' &&
-#sleep 3 &&
+echo -e 'Essa opção não está ativa!!!' &&
+sleep 3 &&
 #veianew
 
 
-echo -e 'Aguarde, conectando no ftp.zanthus.com.br' &&
-echo -e 'Copiando manager mais recente para sua pasta HTDOCS!!!' &&
-php -f 09_baixar_manager.php5 "/Zanthus/Zeus/TESTE/"
+#echo -e 'Aguarde, conectando no ftp.zanthus.com.br' &&
+#echo -e 'Copiando manager mais recente para sua pasta HTDOCS!!!' &&
+#wget -N --tries=2 -P ${PATH_COMUM}/so/    ${FTP_PUB}/Zeus_Retail/WEB/      &&
 
 
+
+##php -f 09_baixar_manager.php5 "/Zanthus/Zeus/TESTE/"
 sleep 3 &&
 
 sleep 1 &&
