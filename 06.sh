e13d65ab8864288070ae5d4bab13ef32196afd33
MarioAlvesCarvalho <mario.alves.souza@outlook.com>
2021-08-31 07:42:26 -0300

    v9.1

--- 06.sh
+++ 06.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#Data alteração 20324
+#Data alteração 21243
 
-echo -e "#REV. 2"
+echo -e "#REV. 3"
 
 echo -e 'Aguarde, conectando no ftp.zanthus.com.br' &&
 echo -e 'Atualizando bibliotecas para o PATH_COMUM!!!' &&
@@ -13,6 +13,7 @@ else
 echo -e "Criando diretorio ${PATH_COMUM}/so"
 mkdir "${PATH_COMUM}/so"
 chmod 777 "${PATH_COMUM}/so"
+chown root.root "${PATH_COMUM}/so"
 fi
 
 if [ -d "${PATH_COMUM}/so_co5" ] ; then
@@ -21,6 +22,7 @@ else
 echo -e "Criando diretorio ${PATH_COMUM}/so_co5"
 mkdir "${PATH_COMUM}/so_co5"
 chmod 777 "${PATH_COMUM}/so_co5"
+chown root.root "${PATH_COMUM}/so_co5"
 fi
 
 if [ -d "${PATH_COMUM}/so_r64" ] ; then
@@ -29,6 +31,7 @@ else
 echo -e "Criando diretorio ${PATH_COMUM}/so_r64"
 mkdir "${PATH_COMUM}/so_r64"
 chmod 777 "${PATH_COMUM}/so_r64"
+chown root.root "${PATH_COMUM}/so_r64"
 fi
 
 if [ -d "${PATH_COMUM}/so_u64" ] ; then
@@ -37,6 +40,7 @@ else
 echo -e "Criando diretorio ${PATH_COMUM}/so_u64"
 mkdir "${PATH_COMUM}/so_u64"
 chmod 777 "${PATH_COMUM}/so_u64"
+chown root.root "${PATH_COMUM}/so_u64"
 fi
 
 if [ -d "${PATH_COMUM}/so_ubu" ] ; then
@@ -45,6 +49,7 @@ else
 echo -e "Criando diretorio ${PATH_COMUM}/so_ubu"
 mkdir "${PATH_COMUM}/so_ubu"
 chmod 777 "${PATH_COMUM}/so_ubu"
+chown root.root "${PATH_COMUM}/so_ubu"
 fi
 
 if [ -d "${PATH_COMUM}/dlls" ] ; then
@@ -53,6 +58,7 @@ else
 echo -e "Criando diretorio ${PATH_COMUM}/dlls"
 mkdir "${PATH_COMUM}/dlls"
 chmod 777 "${PATH_COMUM}/dlls"
+chown root.root "${PATH_COMUM}/dlls"
 fi
 
 wget -N --tries=2 -P ${PATH_COMUM}/so/    ${FTP_COMPLEMENTARES}/so/*      &&
@@ -60,7 +66,12 @@ wget -N --tries=2 -P ${PATH_COMUM}/so_co5 ${FTP_COMPLEMENTARES}/so_co5/*  &&
 wget -N --tries=2 -P ${PATH_COMUM}/so_r64 ${FTP_COMPLEMENTARES}/so_r64/*  &&
 wget -N --tries=2 -P ${PATH_COMUM}/so_u64 ${FTP_COMPLEMENTARES}/so_u64/*  &&
 wget -N --tries=2 -P ${PATH_COMUM}/so_ubu ${FTP_COMPLEMENTARES}/so_ubu/*  &&
-wget -N --tries=2 -P ${PATH_COMUM}/so_ubu ${FTP_COMPLEMENTARES}/dlls/*    &&
+wget -N --tries=2 -P ${PATH_COMUM}/dlls   ${FTP_COMPLEMENTARES}/dlls/*    &&
+
+
+
+
+
 
 sleep 1 &&
 echo -e "" &&
