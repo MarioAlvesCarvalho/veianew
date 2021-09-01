fe33983fde751910d4b939f40bbe21a2187a8fc6
MarioAlvesCarvalho <mario.alves.souza@outlook.com>
2021-09-01 09:36:39 -0300

    9.1.2

--- 52.sh
+++ 52.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 #Data alteração 21244
-#REV. 4
+#REV. 5
 
 if [ -d "${MANAGER}/Kernel/" ] ; then
 echo -e "Validando ${MANAGER}/Kernel/"
 sudo find ${MANAGER}/Kernel/ -iname 'TH*.TR*' -exec rm -rf {} \; &
+echo -e "Conteudo da ${MANAGER}/Kernel/ excluido com sucesso."
+else
+echo -e "Diretorio ${MANAGER}/Kernel/ não existe, nada a ser feito."
 fi
 
 echo -e "" &&
