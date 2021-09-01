672b7dc455ccc96e50653640a295507745e6d42b
MarioAlvesCarvalho <mario.alves.souza@outlook.com>
2021-09-01 09:33:31 -0300

    v9.1.1

--- 52.sh
+++ 52.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-#Data alteração 21242
-#REV. 3
+#Data alteração 21244
+#REV. 4
 
+if [ -d "${MANAGER}/Kernel/" ] ; then
+echo -e "Validando ${MANAGER}/Kernel/"
 sudo find ${MANAGER}/Kernel/ -iname 'TH*.TR*' -exec rm -rf {} \; &
+fi
 
 echo -e "" &&
 echo -e "" &&
