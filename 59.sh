695cf0bbf24419ee57adf58f8d8d87e6303a2e1e
Mario Alves de Souza Carvalho <44899267+MarioAlvesCarvalho@users.noreply.github.com>
2022-09-13 15:44:44 -0300

    Update 59.sh
    #Data alteração 22256
    "#REV. 4"

--- 59.sh
+++ 59.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-#Data alteração 20329
+#Data alteração 22256
 
-echo -e "#REV. 3"
+echo -e "#REV. 4"
 
-rm -rf ${MANAGER}/* &&
+shopt -s extglob
+cd /usr/local/apache22/htdocs/manager &&
+rm -rf !(Arquivos|certificados|ZMWSInfo.ini|exec_cmd_qz.php5|robo_qz.sh) &&
+#rm -rf ${MANAGER}/* &&
 rm -rf ${HTDOCS}/.*.zip.TEMP &&
-cp -rf ${HTDOCS}/certificados ${MANAGER}/ &&
-cp -rf ${HTDOCS}/ZMWSInfo_COMLOG.* ${MANAGER}/ZMWSInfo.ini &
+#cp -rf ${HTDOCS}/certificados ${MANAGER}/ &&
+#cp -rf ${HTDOCS}/ZMWSInfo_COMLOG.* ${MANAGER}/ZMWSInfo.ini &
+cd /usr/local/apache22/htdocs/ &&
 sleep 1 &&
 echo -e "" &&
 echo -e "" &&
@@ -15,3 +19,4 @@ echo -e "****************" &&
 echo -e "Termino do 59.sh" &&
 echo -e "****************" &&
 veianew ;
+
